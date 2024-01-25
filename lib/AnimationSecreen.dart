9f62f6134aeb1370ab7d84ce21199d7e6fd7dcd5
NAGARASU.P <88180695+Nagarasu0408@users.noreply.github.com>
2024-01-25 20:19:53 +0530

    Animated Screen Commit

--- lib/AnimationSecreen.dart
+++ lib/AnimationSecreen.dart
@@ -1,11 +1,17 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
+import 'package:upg/BottomNavigationBar.dart';
 
 import 'package:upg/main.dart';
+import 'package:upg/settingspage.dart';
+
+import 'Theme.dart';
 
 class AnimationScreen extends StatefulWidget {
-  const AnimationScreen({super.key});
+  final ThemeMode preferredTheme;
+  const AnimationScreen({super.key, required this.preferredTheme});
 
   @override
   State<AnimationScreen> createState() => _AnimationScreenState();
@@ -15,6 +21,11 @@ class _AnimationScreenState extends State<AnimationScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: context.watch<ThemeProvider>().darkMode
+          ? ThemeMode.dark
+          : ThemeMode.light,
       debugShowCheckedModeBanner: false,
       home: AnimatedSplashScreen(
         splashIconSize: 450,
@@ -24,18 +35,21 @@ class _AnimationScreenState extends State<AnimationScreen> {
           children: [
 
             Container(
-              child: Lottie.asset('assets/logo.json',height: 200,width: 200),
+              child: Lottie.asset(context.watch<ThemeProvider>().darkMode?'assets/darktheme.json':'assets/logo.json',height: 200,width: 200),
             ),
-            const Text(
+             Text(
               "UPG",
               style: TextStyle(
                   fontSize: 35,
                   fontWeight: FontWeight.w400,
-                  color: Colors.black),
+                  color:context.watch<ThemeProvider>().darkMode? Colors.white: Colors.black),
             ),
           ],
         ),
-        nextScreen: MyApp(),
+        backgroundColor:  context.watch<ThemeProvider>().darkMode ? Colors.black : Colors.white,
+        // nextScreen: MyApp(preferredTheme: ThemeMode.light,),
+        nextScreen: BottomNavigationBarApp(),
+        // nextScreen: SettingsPage(),
         splashTransition: SplashTransition.sizeTransition,
         duration: 1500,
       ),
